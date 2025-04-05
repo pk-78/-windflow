b489fe0e2211d498734e658aef5c7199f45800bd
Abhishek Kumar <skpg0104@gmail.com>
2025-04-05 22:57:22 +0530

    fisrt from abhishek

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,10 @@ function App() {
   return (
     <>
       <h1 className="bg-green-500">hello</h1>
+      <h1 className="bg-green-500">hello</h1>
+      <h1 className="bg-green-500">hello</h1>
+      <h1 className="bg-green-500">hello</h1>
+      <h1 className="bg-green-500">hello</h1>
     </>
   );
 }
